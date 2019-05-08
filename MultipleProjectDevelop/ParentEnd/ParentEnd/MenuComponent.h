cc4b3afabd88996a50ed4cb59d6af383aa9181ef
huangqimeng <huangqimeng@xiaoban.mobi>
2019-05-08 23:04:13 +0800

    wu

---
+++ MultipleProjectDevelop/ParentEnd/ParentEnd/MenuComponent.h
@@ -0,0 +1,17 @@
+//
+//  MenuComponent.h
+//  ParentEnd
+//
+//  Created by keymon on 2019/3/24.
+//  Copyright © 2019 ok. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol MenuComponent <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
