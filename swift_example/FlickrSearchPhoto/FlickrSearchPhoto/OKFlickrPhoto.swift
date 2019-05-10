f21dce96eee81c8694e6e5006833752742200bad
huangqimeng <hqm_vip@163.com>
2019-05-10 14:56:58 +0800

    单利

--- swift_example/FlickrSearchPhoto/FlickrSearchPhoto/OKFlickrPhoto.swift
+++ swift_example/FlickrSearchPhoto/FlickrSearchPhoto/OKFlickrPhoto.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class OKFlickrPhoto: NSObject {
+    
     override init() {
         super.init()
     }
---
+++ swift_example/FlickrSearchPhoto/FlickrSearchPhoto/OKFlickrPhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  OKFlickrPhotoCell.swift
+//  FlickrSearchPhoto
+//
+//  Created by keymon on 2019/5/9.
+//  Copyright © 2019 ok. All rights reserved.
+//
+
+import UIKit
+
+class OKFlickrPhotoCell: UICollectionViewCell {
+    @IBOutlet weak var photoImgV: UIImageView!
+    
+    
+}
