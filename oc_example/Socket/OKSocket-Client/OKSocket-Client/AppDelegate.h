600c203412e74ef822494b46d7a08207067cc7d0
huangqimeng <hqm_vip@163.com>
2019-05-14 17:10:28 +0800

    添加 socket 库

---
+++ oc_example/Socket/OKSocket-Client/OKSocket-Client/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OKSocket-Client
+//
+//  Created by keymon on 2019/5/14.
+//  Copyright © 2019 ok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ oc_example/Socket/OKSocket-Client/OKSocket-Client/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OKSocket-Client
+//
+//  Created by keymon on 2019/5/14.
+//  Copyright © 2019 ok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
