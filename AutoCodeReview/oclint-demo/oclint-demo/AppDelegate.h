e4896fb800d8a40bf0e2f8e78e0b2a91f7844644
huangqimeng <hqm_vip@163.com>
2019-05-19 18:38:14 +0800

    添加 oclint-demo

---
+++ AutoCodeReview/oclint-demo/oclint-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  oclint-demo
+//
+//  Created by keymon on 2019/5/19.
+//  Copyright © 2019 ok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoCodeReview/oclint-demo/oclint-demo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  oclint-demo
+//
+//  Created by keymon on 2019/5/19.
+//  Copyright © 2019 ok. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
