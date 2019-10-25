719de4111564cf6c651056211fed20b6aa607c21
huangqimeng <hqm_vip@163.com>
2019-10-25 18:53:59 +0800

    修改

---
+++ swift_demo/DrawAndAnimation_SwiftUI/DrawAndAnimation_SwiftUI/Drawing/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  DrawAndAnimation_SwiftUI
+//
+//  Created by keymon on 2019/10/25.
+//  Copyright © 2019 olecx. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        BadgeBackground()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
