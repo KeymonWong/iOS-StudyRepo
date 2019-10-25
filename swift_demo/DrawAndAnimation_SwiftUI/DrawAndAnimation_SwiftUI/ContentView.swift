b1e7edc9ef3aac5ef62517a513425948e7138a26
huangqimeng <hqm_vip@163.com>
2019-10-25 16:24:33 +0800

    添加绘制demo

---
+++ swift_demo/DrawAndAnimation_SwiftUI/DrawAndAnimation_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrawAndAnimation_SwiftUI
+//
+//  Created by keymon on 2019/10/25.
+//  Copyright © 2019 olecx. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
