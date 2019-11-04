d7e00afdb425cc8dd7516093f4c2f57591842f7f
huangqimeng <hqm_vip@163.com>
2019-11-04 13:52:19 +0800

    完成 drawing

--- swift_demo/DrawAndAnimation_SwiftUI/DrawAndAnimation_SwiftUI/Drawing/Badge.swift
+++ swift_demo/DrawAndAnimation_SwiftUI/DrawAndAnimation_SwiftUI/Drawing/Badge.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 struct Badge: View {
+    static let rotationCount = 8
+    
+    var badgeSymbols: some View {
+        ForEach(0 ..< Self.rotationCount) { i in
+            RotatedBadgeSymbol(angle: .degrees(Double(i) / Double(Self.rotationCount)) * 360.0)
+        }
+        .opacity(0.5)
+    }
+    
     var body: some View {
-        BadgeBackground()
+        ZStack {
+            BadgeBackground()
+            
+            GeometryReader { geo in
+                self.badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geo.size.width / 2.0, y: (3.0 / 4.0) * geo.size.height)
+            }
+        }
+        .scaledToFit()
     }
 }
 
---
+++ swift_demo/DrawAndAnimation_SwiftUI/DrawAndAnimation_SwiftUI/Drawing/BadgeSymbol.swift
@@ -0,0 +1,49 @@
+//
+//  BadgeSymbol.swift
+//  DrawAndAnimation_SwiftUI
+//
+//  Created by keymon on 2019/11/4.
+//  Copyright © 2019 olecx. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    static let symbolColor = Color(red: 79.0 / 255, green: 79.0 / 255, blue: 191.0 / 255)
+    
+    var body: some View {
+        GeometryReader { geo in
+            Path { path in
+                let width = min(geo.size.width, geo.size.height)
+                let height = width * 0.75
+                let spacing = width * 0.030
+                let middle = width / 2
+                let topWidth = 0.226 * width
+                let topHeight = 0.488 * height
+                
+                path.addLines([
+                    CGPoint(x: middle, y: spacing),
+                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
+                    CGPoint(x: middle, y: spacing)
+                ])
+                path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
+                path.addLines([
+                    CGPoint(x: middle - topWidth, y: topHeight + spacing),
+                    CGPoint(x: spacing, y: height - spacing),
+                    CGPoint(x: width - spacing, y: height - spacing),
+                    CGPoint(x: middle + topWidth, y: topHeight + spacing),
+                    CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
+                ])
+            }
+            .fill(Self.symbolColor)
+        }
+    }
+}
+
+struct BadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeSymbol()
+    }
+}
