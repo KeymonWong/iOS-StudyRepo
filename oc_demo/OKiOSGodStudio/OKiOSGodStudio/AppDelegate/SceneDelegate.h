0b6886f065010699e83b0540d67fa0998286b609
huangqimeng <hqm_vip@163.com>
2019-11-18 21:01:23 +0800

    集合

---
+++ oc_demo/OKiOSGodStudio/OKiOSGodStudio/AppDelegate/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  OKiOSGodStudio
+//
+//  Created by keymon on 2019/11/18.
+//  Copyright © 2019 okay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ oc_demo/OKiOSGodStudio/OKiOSGodStudio/Root/RootViewController.m
@@ -0,0 +1,23 @@
+//
+//  RootViewController.m
+//  OKiOSGodStudio
+//
+//  Created by keymon on 2019/11/18.
+//  Copyright © 2019 okay. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
